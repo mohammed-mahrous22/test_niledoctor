fdd4613c9353a91199fa46d800d9c3f3c6c11d0b
xxspider4 <xxspider44@gmail.com>
2020-12-22 16:32:30 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['Adminauth','auth'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
